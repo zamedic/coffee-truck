2a3e3da850220829c1d65a62770a4d7c021ec0ce
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-04 09:00:18 +0200

    each => map

--- libraries/coffee_truck_helper.rb
+++ libraries/coffee_truck_helper.rb
@@ -11,7 +11,7 @@ module CoffeeTruck
       cwd = node['delivery']['workspace']['repo']
       command = "curl -X GET '#{node['delivery']['config']['sonar']['host']}/api/resources?resource=#{node['delivery']['config']['sonar']['resource']}&metrics=ncloc,coverage,tests,test_errors,test_failures'"
       raw = JSON.parse `cd #{cwd} && #{command}`
-      metrics = raw[0]['msr'].each do |msr|
+      metrics = raw[0]['msr'].map do |msr|
         [msr['key'], msr['val']]
       end.to_h
       {
