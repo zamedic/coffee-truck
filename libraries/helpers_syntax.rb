9440386461245cfc50847fd996e31f4bf943de5e
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-09 10:16:59 +0200

    Add change helper

---
+++ libraries/helpers_syntax.rb
@@ -0,0 +1,21 @@
+module CoffeeTruck
+  module Helpers
+    module Syntax
+      extend self
+
+      def bumped_version?(path, node)
+        change = DeliverySugar::Change.new(node)
+        modified_files = change.changed_files
+        Chef::Log.error("============\n#{modified_files}\n==============")
+        raise Error
+      end
+    end
+  end
+
+  module DSL
+
+    def bumped_version?(path)
+      CoffeeTruck::Helpers::Syntax.bumped_version?(path, node)
+    end
+  end
+end
--- recipes/syntax.rb
+++ recipes/syntax.rb
@@ -1,14 +1,12 @@
 include_recipe 'delivery-truck::syntax'
 
 
-#  # If we changed a cookbook but didn't bump the version than the build
-#  # phase will fail when trying to upload to the Chef Server.
-#  unless bumped_version?(cookbook.path)
-#    raise RuntimeError, "The #{cookbook.name} cookbook was modified " \
-#                        "but the version was not updated in the " \
-#                        "metadata file."
-#  end 
-#
+# If we changed a cookbook but didn't bump the version than the build
+# phase will fail when trying to upload to the Chef Server.
+unless bumped_version?(node['delivery']['workspace']['repo'])
+  raise RuntimeError, "Artefact versino unchanged"
+end 
+
 #  # Run `knife cookbook test` against the modified cookbook
 #  execute "syntax_check_#{cookbook.name}" do
 #    command "knife cookbook test -o #{cookbook.path} -a"
