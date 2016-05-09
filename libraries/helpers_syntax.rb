820a5e4d0ddbbf5f0b5db39dcda22a4017394fd8
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-09 15:31:03 +0200

    Add file_changes

--- libraries/helpers_syntax.rb
+++ libraries/helpers_syntax.rb
@@ -7,7 +7,15 @@ module CoffeeTruck
         change = DeliverySugar::Change.new(node)
         modified_files = change.changed_files
         Chef::Log.error("============\n#{modified_files}\n==============")
+        file_changes
         raise RuntimeError, "=========\n#{modified_files}\n========="
+      end
+
+      def file_changes()
+        ref1 = "origin/#{@pipeline}"
+        ref2 = "origin/#{@patchset_branch}"
+        results = shell_out!("git diff --name-only #{ref1} #{ref2}", cwd: workspace).stdout.chomp.split("\n")
+        Chef::Log.error("++++++++++++++++\n#{results}\n++++++++++++++++++")
       end
     end
   end
