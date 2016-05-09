f9981dcb5a9acae8b974ab3f9eac2dbb1066925d
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-09 10:21:16 +0200

    Raise exception

--- recipes/syntax.rb
+++ recipes/syntax.rb
@@ -4,9 +4,9 @@ include_recipe 'delivery-truck::syntax'
 # If we changed a cookbook but didn't bump the version than the build
 # phase will fail when trying to upload to the Chef Server.
 unless bumped_version?(node['delivery']['workspace']['repo'])
-  raise RuntimeError, "Artefact versino unchanged"
 end 
 
+raise RuntimeError, "Artefact versino unchanged"
 #  # Run `knife cookbook test` against the modified cookbook
 #  execute "syntax_check_#{cookbook.name}" do
 #    command "knife cookbook test -o #{cookbook.path} -a"
