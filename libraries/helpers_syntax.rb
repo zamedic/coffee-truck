ad66865bd74e1b5eb5a4b0ed2a6bc78d516d08c4
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-09 16:01:09 +0200

    Show file at ref1

--- libraries/helpers_syntax.rb
+++ libraries/helpers_syntax.rb
@@ -19,7 +19,8 @@ module CoffeeTruck
         ref1 = "origin/#{change.pipeline}"
         ref2 = "origin/#{change.patchset_branch}"
         Chef::Log.error("Ref1: #{ref1}\nRef2: #{ref2}\n#{change.workspace_repo}")
-        results = shell_out!("git diff --name-only #{ref1} #{ref2}", cwd: change.workspace_repo).stdout.chomp.split("\n")
+        #results = shell_out!("git diff #{ref1} #{ref2}", cwd: change.workspace_repo).stdout.chomp.split("\n")
+        results = shell_out!("git show #{ref1}:pom.xml", cwd: change.workspace_repo).stdout.chomp.split("\n")
         Chef::Log.error("++++++++++++++++\n#{results}\n++++++++++++++++++")
       end
     end
