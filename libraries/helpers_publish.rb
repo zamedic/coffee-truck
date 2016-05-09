b427f25556c757102b36cf3d6e97fa80b6460e2a
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-09 20:13:54 +0200

    publish extend self

--- libraries/helpers_publish.rb
+++ libraries/helpers_publish.rb
@@ -1,6 +1,8 @@
 module CoffeeTruck
   module Helpers
     module Publish
+      extend self
+
       def gitlog(node)
         cwd = node['delivery']['workspace']['repo']
         command = 'git log --numstat --pretty="%H" --since="1 month ago" | awk \'NF==3 {a[$3]+=$1+$2} END {for (i in a) printf("%5d\t%s\n", a[i], i)}\' | grep .java$ | sort -k1 -rn'
