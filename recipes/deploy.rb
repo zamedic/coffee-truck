680b4db5b753599729d8836125c2c71ed02b9d38
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-10 18:45:23 +0200

    Add logging to deploy

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -2,9 +2,12 @@ search_query = "(#{node['delivery']['config']['truck']['recipe']}) " \
                "AND chef_environment:#{delivery_environment} " \
                "AND #{deployment_search_query}"
 
+Chef::Log.error(search_query)
 my_nodes = delivery_chef_server_search(:node, search_query)
+Chef::Log.error(my_nodes)
 
 my_nodes.map!(&:name)
+Chef::Log.error(my_nodes.join(", "))
 
 delivery_push_job "deploy_#{node['delivery']['change']['project']}" do
   command 'chef-client'
