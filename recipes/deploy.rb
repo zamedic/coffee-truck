dcc3f9b8f6b25d0c83b0b523200859551f31c5c1
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-10 18:33:20 +0200

    Add nodes to deploy search

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,1 +1,12 @@
-include_recipe 'delivery-truck::deploy'
+search_query = "(#{node['delivery']['config']['truck']['recipe']}) " \
+               "AND chef_environment:#{delivery_environment} " \
+               "AND #{deployment_search_query}"
+
+my_nodes = delivery_chef_server_search(:node, search_query)
+
+my_nodes.map!(&:name)
+
+delivery_push_job "deploy_#{node['delivery']['change']['project']}" do
+  command 'chef-client'
+  nodes my_nodes
+end
