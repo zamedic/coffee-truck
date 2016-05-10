4dab96036bd477554acd9a8f01fc2ecb5fb83e18
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-10 08:10:11 +0200

    Set defaults for hashes

--- libraries/mvn_resource.rb
+++ libraries/mvn_resource.rb
@@ -9,8 +9,8 @@ class Chef
       attribute :name, :kind_of => String, required: true, name_attribute: true
       attribute :cwd, :kind_of => String
       attribute :settings, :kind_of => String
-      attribute :definitions, :kind_of => Hash
-      attribute :environment, :kind_of => Hash
+      attribute :definitions, :kind_of => Hash, default: Hash.new
+      attribute :environment, :kind_of => Hash, default: Hash.new
     end
   end
 end
