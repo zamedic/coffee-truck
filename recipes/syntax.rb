1c0075b0a10610c185b26438396d5302d37be1e8
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-05-18 09:35:05 +0200

    Temporarily disable version check

--- recipes/syntax.rb
+++ recipes/syntax.rb
@@ -1,6 +1,6 @@
 include_recipe 'delivery-test::syntax'
 
 
-unless bumped_pom_version?(node['delivery']['workspace']['repo'])
-  raise RuntimeError, "Artifact version unchanged - you have to increase the version in the pom file"
-end 
+#unless bumped_pom_version?(node['delivery']['workspace']['repo'])
+#  raise RuntimeError, "Artifact version unchanged - you have to increase the version in the pom file"
+#end 
