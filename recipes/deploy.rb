5fd109d43f3a3ab0597638396797188534463d5c
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-06-30 18:29:10 +0200

    Update acceptance env search

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,5 +1,8 @@
+current_env = delivery_environment
+current_env = 'acceptance-*' if current_env.start_with? 'acceptance-'
+
 search_query = "recipes:#{node['delivery']['config']['truck']['recipe']} " \
-               "AND chef_environment:#{delivery_environment} " \
+               "AND chef_environment:#{current_env} " \
                "AND #{deployment_search_query}"
 
 my_nodes = delivery_chef_server_search(:node, search_query)
