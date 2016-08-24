d392abc2da03e8e99b6f0b3420c44d3d8f1c3514
Marc Arndt <marc@marcarndt.com>
2016-08-24 07:42:35 +0200

    fixing skip test on release prepare

--- libraries/mvn_provider.rb
+++ libraries/mvn_provider.rb
@@ -50,7 +50,7 @@ class  Chef
         command_pull = "git pull"
         command_email = "git config user.email 'delivery@standardbank.co.za'"
         command_user = "git config user.name 'Delivery Server'"
-        command = "mvn -B release:prepare -Dmaven.test.skip=true -DupdateWorkingCopyVersions=false -DsuppressCommitBeforeTagOrBranch=true #{args}"
+        command = "mvn -B release:prepare -Darguments='-Dmaven.test.skip=true' -DupdateWorkingCopyVersions=false -DsuppressCommitBeforeTagOrBranch=true #{args}"
         converge_by "Preparing Release: #{command}" do
           exec command_email
           exec command_user
