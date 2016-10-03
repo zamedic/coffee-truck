b68c8445afaf355a8c4e4ed4bbf48c33b091c24a
Stephen Breyer-Menke <steve.bm@gmail.com>
2016-10-03 14:46:41 +0200

    Update test-results

--- libraries/helpers_unit.rb
+++ libraries/helpers_unit.rb
@@ -5,7 +5,7 @@ module CoffeeTruck
 
       def sonarmetrics(node)
         cwd = node['delivery']['workspace']['repo']
-        command = "curl -X GET '#{node['delivery']['config']['sonar']['host']}/api/resources?resource=#{node['delivery']['config']['sonar']['resource']}&metrics=ncloc,coverage,tests,test_errors,test_failures'"
+        command = "curl -X GET '#{node['delivery']['config']['sonar']['host']}/api/resources?resource=#{node['delivery']['config']['sonar']['resource']}&metrics=coverage,tests,test_errors,test_failures'"
         begin
           raw = JSON.parse `cd #{cwd} && #{command}`
           metrics = raw[0]['msr'].map do |msr|
@@ -15,16 +15,11 @@ module CoffeeTruck
           metrics = {}
         end
         {
-          lines: metrics['ncloc'],
           coverage: metrics['coverage'],
           unit: {
             total: metrics['tests'],
             errors: metrics['test_errors'],
             failures: metrics['test_failures']
-          },
-          integration: {
-          },
-          acceptance: {
           }
         }
       end
