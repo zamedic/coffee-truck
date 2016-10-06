2552634e195e9f7d7b81c719fb4892eaa2b66109
Marc Arndt <marc@marcarndt.com>
2016-10-06 12:19:35 +0200

    moving demoncat update to after publish. If demoncat updates for any reason the pipeline wont be broken

--- recipes/unit.rb
+++ recipes/unit.rb
@@ -8,19 +8,6 @@ mvn 'sonar' do
   definitions('sonar.host.url' => node['delivery']['config']['sonar']['host'])
 end
 
-#http_request 'sonar-results' do
-#  action :post
-#  url 'http://spambot.standardbank.co.za/events/test-results'
-#  # ignore_failure true
-#  headers('Content-Type' => 'application/json')
-#  message lazy {
-#    {
-#      application: node['delivery']['config']['truck']['application'],
-#      results: sonarmetrics(node)
-#    }.to_json
-#  }
-#end
-
 #Upload Snapshot
 mvn 'upload' do
   action :upload
