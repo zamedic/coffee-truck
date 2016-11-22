c3a2ac93fa355ea637d5d2085067819132254d15
Marc Arndt <marc@marcarndt.com>
2016-11-22 15:03:13 +0200

    fixing snapshot

--- recipes/syntax.rb
+++ recipes/syntax.rb
@@ -5,6 +5,6 @@ unless bumped_pom_version?(node['delivery']['workspace']['repo'])
   raise RuntimeError, "Artifact version unchanged - you have to increase the version in the pom file"
 end 
 
-unless ensure_shapshot?(node['delivery']['workspace']['repo'])
+unless ensure_snapshot?(node['delivery']['workspace']['repo'])
   raise RuntimeError, "-SNAPSHOT artifact required in your pom.xml file"
 end
