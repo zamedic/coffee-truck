dd62db165d4e21b22f0205b864636e3f7060e83c
Nkululeko <nkululeko.selepe@standardbank.co.za>
2016-12-01 10:58:45 +0200

    added some stuff

--- recipes/lint.rb
+++ recipes/lint.rb
@@ -1,1 +1,22 @@
 include_recipe 'delivery-truck::lint'
+
+mvn 'lint' do
+  action :lint
+end
+
+mvn 'sonar' do
+  action :sonar
+  definitions('sonar.host.url' => node['delivery']['config']['sonar']['host'])
+end
+
+http_request 'lint-test-results' do
+  action :post
+  url 'http://spambot.standardbank.co.za/events/lint-results'
+  headers('Content-Type' => 'application/json')
+  message lazy {
+    {
+      application: node['delivery']['config']['truck']['application'],
+      results: sonermetrics(node)
+    }.to_json
+  }
+end
--- recipes/security.rb
+++ recipes/security.rb
@@ -1,1 +1,23 @@
 include_recipe 'delivery-truck::security'
+
+mvn 'security' do
+  action :security
+end
+
+mvn 'sonar' do
+  action :sonar
+  definitions('sonar.host.url' => node['delivery']['config']['sonar']['host'])
+end
+
+http_request 'security-test-results' do
+  action :post
+  url 'http://spambot.standardbank.co.za/events/security-results'
+  headers('Content-Type' => 'application/json')
+  message lazy {
+    {
+      application: node['delivery']['config']['truck']['application'],
+      results: sonermetrics(node)
+    }.to_json
+  }
+end
+
