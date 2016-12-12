aebf0bbc720890738891d5d3231995f3c1943652
Marc Arndt <marc@marcarndt.com>
2016-12-12 18:16:07 +0200

    adding complexity checks

--- recipes/publish.rb
+++ recipes/publish.rb
@@ -50,5 +50,18 @@ http_request 'lint-results' do
   }
 end
 
+http_request 'complexity-results' do
+  action :post
+  url 'http://spambot.standardbank.co.za/events/quality-results'
+  ignore_failure true
+  headers('Content-Type' => 'application/json')
+  message lazy {
+    {
+        application: node['delivery']['config']['truck']['application'],
+        results: current_complexity(node)
+    }.to_json
+  }
+end
+
 
 
