7382b4f10494cb2b8f0870c754a5ae69be27aa33
Marc Arndt <marc@marcarndt.com>
2016-12-12 08:34:15 +0200

    unit test coverage checking and enforcement

--- recipes/unit.rb
+++ recipes/unit.rb
@@ -2,21 +2,9 @@ mvn 'unit' do
   action :unit
 end
 
+#Check Unit Tests
 mvn 'jacoco' do
   action :jacoco_report
-end
-
-http_request 'sonar-results' do
-  ignore_failure true
-  action :post
-  url 'http://spambot.standardbank.co.za/events/test-results'
-  headers('Content-Type' => 'application/json')
-  message lazy {
-    {
-      application: node['delivery']['config']['truck']['application'],
-      results: sonarmetrics(node)
-    }.to_json
-  }
 end
 
 #Upload Snapshot
