64ef6ed0033d7f8ccb8bc44b1e6e650a9c6a25fc
Marc Arndt <marc@marcarndt.com>
2016-12-12 09:53:17 +0200

    lint and pmb enablement

--- libraries/helpers_lint.rb
+++ libraries/helpers_lint.rb
@@ -8,15 +8,16 @@ module CoffeeTruck
       include Chef::Mixin::ShellOut
       extend self
 
-      def count_violations(node)
+      def count_pmd_violations(node)
         file = "#{node['delivery']['workspace']['repo']}/target/pmd.xml"
         doc = ::File.open(file) { |f| Nokogiri::XML(f) }
-        doc.xpath("count(//violation)").first.text.to_i
+        doc.xpath("count(//violation)").to_i
       end
 
       def check_pmd?(node)
         Chef::Log.error(count_violations(node))
       end
+
     end
   end
 
--- recipes/lint.rb
+++ recipes/lint.rb
@@ -3,3 +3,18 @@ include_recipe 'delivery-truck::lint'
 mvn 'pmd' do
   action :pmd
 end
+
+http_request 'sonar-results' do
+  action :post
+  url 'http://spambot.standardbank.co.za/events/lint-results'
+  ignore_failure true
+  headers('Content-Type' => 'application/json')
+  message lazy {
+    {
+        application: node['delivery']['config']['truck']['application'],
+        results: {
+            issues: count_pmd_violations(node)
+        } 
+    }.to_json
+  }
+end
