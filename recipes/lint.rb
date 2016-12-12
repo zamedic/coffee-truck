f2bfbbe383e1b0526994561e611362c75a9bd7f1
Marc Arndt <marc@marcarndt.com>
2016-12-12 16:29:55 +0200

    adding complexity checks

--- recipes/lint.rb
+++ recipes/lint.rb
@@ -1,18 +1,11 @@
 include_recipe 'delivery-truck::lint'
 
+mvn 'complexity' do
+  action :checkstyle
+end
+
 mvn 'pmd' do
   action :pmd
 end
 
-cookbook_file "/tmp/checkstyle.xml" do
-  source 'settings.xml'
-  owner 'dbuild'
-  group 'root'
-  mode 00644
-  action :create
-end
-
-mvn 'complexity' do
-  action :checkstyle
-end
 
