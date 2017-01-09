c1496bd9687512a929c0530f2207601ae76730f8
Marc Arndt <marc@marcarndt.com>
2017-01-09 09:26:49 +0200

    fixing host file entry

--- recipes/default.rb
+++ recipes/default.rb
@@ -31,7 +31,7 @@ end
 
 hostsfile_entry '127.0.0.1' do
   hostname 'accstandardbank.d1.sc.omtrdc.net'
-  aliases ['lar.standardbank.co.za','rwp.standardbank.co.za','cdn.standardbank.co.za','dfib.standardbank.co.za','dspk.standardbank.co.za','trk.standardbank.co.za']
+  aliases ['lar.standardbank.co.za','rwp.standardbank.co.za','cdn.standardbank.co.za','dfib.standardbank.co.za','dspk.standardbank.co.za','trk.standardbank.co.za','localhost.localdomain','localhost']
   action :create
 end
 
