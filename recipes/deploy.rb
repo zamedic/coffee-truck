3ca58264f9dcd9f49961f4db88c8d356ca9b1d29
Marc Arndt <marc@marcarndt.com>
2017-01-16 11:10:19 +0200

    since the deploy recipe is now part of the project, we shouldnt need a specic deploy step anymore

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,16 +1,3 @@
 include_recipe 'delivery-truck::deploy'
 
-current_env = delivery_environment
-current_env = 'acceptance-*' if current_env.start_with? 'acceptance-'
 
-search_query = "recipes:#{node['delivery']['config']['truck']['recipe']} " \
-               "AND chef_environment:#{current_env} " \
-               "AND #{deployment_search_query}"
-
-my_nodes = delivery_chef_server_search(:node, search_query)
-my_nodes.map!(&:name)
-
-delivery_push_job "deploy_#{node['delivery']['change']['project']}" do
-  command 'chef-client'
-  nodes my_nodes
-end
