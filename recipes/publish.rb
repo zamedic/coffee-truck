18449c2adcfb8f25238c4212b3cc628f3e7489ce
Marc Arndt <marc@marcarndt.com>
2017-01-16 11:16:27 +0200

    removing unused git history publish

--- recipes/publish.rb
+++ recipes/publish.rb
@@ -1,16 +1,3 @@
-http_request 'files-changed' do
-  action :post
-  url 'http://spambot.standardbank.co.za/events/gitlog'
-  ignore_failure true
-  headers('Content-Type' => 'application/json')
-  message lazy {
-    {
-        application: node['delivery']['config']['truck']['application'],
-        changes: gitlog(node)
-    }.to_json
-  }
-end
-
 mvn 'release_prepare' do
   action :release_prepare
 end
