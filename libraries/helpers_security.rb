53ae2d96043484d52890826db951df1eeddc2fe8
Marc Arndt <marc@marcarndt.com>
2017-01-19 12:15:40 +0200

    security tests

--- libraries/helpers_security.rb
+++ libraries/helpers_security.rb
@@ -10,8 +10,9 @@ module CoffeeTruck
         http = Net::HTTP.new('psdc-pa001gth1v.za.sbicdirectory.com','443')
         http.use_ssl = true
         request = Net::HTTP::Get.new('/Cxwebinterface/odata/v1/Projects?$expand=LastScan&$orderby=LastScan/RiskScore%20desc&$top=2')
-        request.ntlm_auth('c1592023', '', 'trendweb')
+        request.ntlm_auth('c1592023', 'sbicza01', 'trendweb')
         response = http.request(request)
+        Chef::Log.warn(http)
         Chef::Log.warn(response)
       end
 
