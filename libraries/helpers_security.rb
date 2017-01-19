a48de7a7775f00e61f2471949afd0f4f687c2f45
Marc Arndt <marc@marcarndt.com>
2017-01-19 10:28:48 +0200

    security tests

--- libraries/helpers_security.rb
+++ libraries/helpers_security.rb
@@ -1,4 +1,4 @@
-require 'rubyntlm'
+require 'httpi-ntlm'
 
 
 module CoffeeTruck
@@ -8,12 +8,11 @@ module CoffeeTruck
       extend self
 
       def getSecurityStats()
-        request = URI("https://psdc-pa001gth1v.za.sbicdirectory.com/Cxwebinterface/odata/v1/Projects?$expand=LastScan&$orderby=LastScan/RiskScore%20desc&$top=2")
-        message_from_server = Net::HTTP.get(request)
-        Chef::Log.warn("Message from server #{message_from_server}")
-        t2 = Net::NTLM::Message.parse(message_from_server)
-        t3 = t2.response({:user => 'c1592023', :password => 'trendweb'})
-        Chef::Log.warn("NTLM Message #{t3}")
+
+        request = HTTPI::Request.new("https://psdc-pa001gth1v.za.sbicdirectory.com/Cxwebinterface/odata/v1/Projects?$expand=LastScan&$orderby=LastScan/RiskScore%20desc&$top=2")
+        request.auth.ntlm("c1592023", "trendweb")
+        response = HTTPI.get request
+        Chef::Log.warn(response.body)
 
       end
 
