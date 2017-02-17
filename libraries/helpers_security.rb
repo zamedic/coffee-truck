76d16edab4dcf65e1df0f6cae1d55cd242f00a73
Marc Arndt <marc@marcarndt.com>
2017-02-17 13:54:25 +0200

    ignore ssl errors

--- libraries/helpers_security.rb
+++ libraries/helpers_security.rb
@@ -7,6 +7,7 @@ module CoffeeTruck
       def getSecurityStats(node)
         http = Net::HTTP.new('10.144.20.96','443')
         http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         request = Net::HTTP::Get.new(node['delivery']['config']['truck']['security_url'])
         request.add_field('X-IBM-Client-Id','945b4987-8857-43a1-8038-bf4753cc6b7f')
         response = http.request(request)
@@ -29,7 +30,7 @@ module CoffeeTruck
         req.body = body
         req.content_type = 'application/json'
 
-        Net::HTTP.start(uri.hostname, uri.port,:use_ssl => uri.scheme == 'https',:verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+        Net::HTTP.start(uri.hostname, uri.port) do |http|
           http.request(req)
         end
       end
