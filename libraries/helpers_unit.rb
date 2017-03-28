ec23372e80f3f3513e8a2e9a56b509902667b7d8
Marc Arndt <marc.arndt@standardbank.co.za>
2017-03-28 12:37:51 +0200

    adding logging for error node

--- libraries/helpers_unit.rb
+++ libraries/helpers_unit.rb
@@ -109,13 +109,11 @@ module CoffeeTruck
           runtime = testcase.xpath("@time").first.text.to_f
           name = testcase.xpath("@name").first.text
           class_name = testcase.xpath("@classname").first.text
-          errors = testcase.xpath("/failure")
-          Chef::Log.warn("Error? #{error}")
 
           if (runtime > 3)
             Chef::Log.warn("Runtime for test #{name} in class #{class_name} has a runtime of #{runtime}, this exceeded the 3 second threshold. This is probably not a valid unit test")
           end
-          errors.each { |error|
+          testcase.xpath("/failure").each { |error|
             Chef::Log.warn("the following error was encountered with unit test #{name} in class #{class_name}. #{error.xpath('@message')}")
             failed = true
           }
