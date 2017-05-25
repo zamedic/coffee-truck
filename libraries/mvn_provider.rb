51e7c8f9bda9cb55172b8402b11d50346a15b8af
Marc Arndt <marc@marcarndt.com>
2017-05-25 08:39:12 +0200

    adding path logging

--- libraries/mvn_provider.rb
+++ libraries/mvn_provider.rb
@@ -149,13 +149,14 @@ class Chef
       end
 
       def exec(command)
-        Chef::Log.warn("Path: #{ENV['PATH']} ")
+
         options = Hash.new
         options[:cwd] = @new_resource.cwd || node['delivery']['workspace']['repo']
         options[:timeout] = 1200
         options[:environment] = {
             'PATH' => "#{node['maven']['m2_home']}-#{node['maven']['version']}/bin:#{ENV['PATH']}", "DISPLAY" => ":10"
         }.merge @new_resource.environment
+        Chef::Log.warn("Path: #{options[:environment]} ")
         out = shell_out!(command, options)
         Chef::Log.warn("Exit Status: #{out.exitstatus} ")
         if out.exitstatus != 0
