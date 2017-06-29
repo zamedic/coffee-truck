07b78b0da09b5e555a287f680b79b372fa3b8839
Marc Arndt <marc@marcarndt.com>
2017-06-29 13:08:47 +0200

    fixing spelling of codacy

--- recipes/unit.rb
+++ recipes/unit.rb
@@ -2,7 +2,7 @@ include_recipe 'delivery-truck::unit'
 
 if (java_changes?(changed_files))
 
-  if(node['delivery']['change']['stage'] == 'verify' && node['delivery']['config']['truck']['update_dependencies']['active'])
+  if (node['delivery']['change']['stage'] == 'verify' && node['delivery']['config']['truck']['update_dependencies']['active'])
     mvn 'bumpDependencies' do
       action :updateDependencies
     end
@@ -17,18 +17,17 @@ if (java_changes?(changed_files))
     action :jacoco_report
   end
 
-  if(node['delivery']['config']['truck']['codacy']['upload'] && node['delivery']['change']['stage']=='build')
+  if (node['delivery']['config']['truck']['codacy']['upload'] && node['delivery']['change']['stage']=='build')
     remote_file '/tmp/codacy.jar' do
       source 'https://github.com/codacy/codacy-coverage-reporter/releases/download/2.0.0/codacy-coverage-reporter-2.0.0-assembly.jar'
     end
 
-    execute 'java -cp /tmp/codacy.jar com.codacy.CodacyCoverageReporter -l Java -r ./repo/target/jacoco.xml' do
-      environment ({'CODACY_PROJECT_TOKEN' => node['delivery']['config']['truck']['codacy']['token']})
-    end
+    execute "java -cp /tmp/codacy.jar com.codacy.CodacyCoverageReporter -l Java -r ./repo/target/jacoco.xml --projectToken #{node['delivery']['config']['truck']['codacy']['token']}"
+   
   end
 
 #Upload Snapshot
-  if(node['delivery']['config']['truck']['maven']['upload_snapshot'])
+  if (node['delivery']['config']['truck']['maven']['upload_snapshot'])
     mvn 'upload' do
       action :upload
     end
