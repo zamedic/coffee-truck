5b81d270e6341de3d1eda53e65d871efbf41fca4
Marc Arndt <marc@marcarndt.com>
2017-07-05 14:31:48 +0200

    switching to return java changes if any src files have changed

--- libraries/helpers_syntax.rb
+++ libraries/helpers_syntax.rb
@@ -35,7 +35,7 @@ module CoffeeTruck
       def java_changes?(node,changes)
         Chef::Log.warn(changes)
         changes.each do |file|
-          if (!file.start_with?('cookbooks/','.delivery/','.gitignore','README.md','Dockerfile'))
+          if (file.start_with?('/src'))
             return true
           end
         end
